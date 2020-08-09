2c8929833a3ca73dc547e97ae44af7fc33f00868
Gabriel Ewig <gabriel@cyclikal.com>
2020-08-09 15:02:08 -0500

    Standardize object names

--- cyp_mettler_ag204/cyp_mettler_ag204.py
+++ cyp_mettler_ag204/cyp_mettler_ag204.py
@@ -6,8 +6,8 @@ import os.path
 logger = logging.getLogger("cyckei")
 
 
-class MettlerController(cyp_base.PluginController):
-    def __init__(self):
+class PluginController(cyp_base.BaseController):
+    def __init__(self, sources):
         base_path = os.path.join(os.path.dirname(__file__), "..")
         super().__init__("mettler-ag204", base_path)
 
@@ -23,13 +23,13 @@ class MettlerController(cyp_base.PluginController):
         # Sources don't need to be found for this plugin,
         # so we just initialize two randomizers as examples
         sources = {}
-        sources["Rand1"] = MettlerScale(10)
-        sources["Rand2"] = MettlerScale(20)
+        sources["Rand1"] = PluginSource(10)
+        sources["Rand2"] = PluginSource(20)
 
         return sources
 
 
-class MettlerScale(cyp_base.SourceObject):
+class PluginSource(cyp_base.BaseSource):
     def __init__(self, port):
         super().__init__()
         self.range = [0, port]
@@ -40,5 +40,5 @@ class MettlerScale(cyp_base.SourceObject):
 
 
 if __name__ == "__main__":
-    controller = MettlerController()
+    controller = PluginController()
     print(cyp_base.read_all(controller))
